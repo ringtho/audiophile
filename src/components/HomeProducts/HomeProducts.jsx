b19b7f6a059d684beebc1c8d90192ad652e2c71c
Smith Ringtho <sringtho@gmail.com>
2023-06-12 21:08:49 +0300

    Added ZX( speaker banner

--- src/components/HomeProducts/HomeProducts.jsx
+++ src/components/HomeProducts/HomeProducts.jsx
@@ -1,15 +1,25 @@
 import React from 'react'
 import './HomeProducts.scss'
 import { Button } from '../index'
+import images from '../../constants/images'
 
 const HomeProducts = () => {
   return (
     <div className='products__container'>
         <div className='products__wrapper'>
-            <div className='products__details'>
-                <h2 className='products__title'>ZX9 SPEAKER</h2>
-                <p>Upgrade to premium speakers that are phenomenally built to deliver truly remarkable sound.</p>
-                <Button text='see product' />
+            <div className='products__circles'>
+              <img src={images.homeCircles} alt='circles' />
+            </div>
+
+            <div className='products'>
+              <div className='products__image'>
+                <img src={images.headphonesZX9} alt='product-image' />
+              </div>
+              <div className='products__details'>
+                  <h2 className='products__title'>ZX9 <br/>SPEAKER</h2>
+                  <p>Upgrade to premium speakers that are phenomenally built to deliver truly remarkable sound.</p>
+                  <Button text='see product' />
+              </div>
             </div>
         </div>
     </div>
