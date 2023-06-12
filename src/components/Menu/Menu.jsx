b535f0861aa04dab3b438034e0c2dc8fe135d388
Smith Ringtho <sringtho@gmail.com>
2023-06-12 06:56:32 +0000

    Added a few components of the menu item

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import './Menu.scss'
+import { Link } from 'react-router-dom'
+import images from '../../constants/images'
 
 const Menu = () => {
   return (
-    <div>
-      Menu
+    <div className='menu__wrapper'>
+      <div className='menu__container'>
+        <div className='menu__thumbnail'>
+          <img src={images.earphonesCategory} alt='category' />
+        </div>
+        <div className='menu__info'>
+          <div className='menu__info-container'>
+            <h3>Speakers</h3>
+            <Link className='menu__link' to=''>
+              <h4>SHOP</h4>
+              <img src={images.arrowRight} alt='arrow-right' className='arrow-right' />
+            </Link>
+           </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/constants/images.js
+++ src/constants/images.js
@@ -8,6 +8,11 @@ import cart from '../assets/shared/desktop/icon-cart.svg'
 import heroMobile from '../assets/home/mobile/image-header.jpg'
 import heroTablet from '../assets/home/tablet/image-header.jpg'
 import hero from '../assets/home/tablet/hero.webp'
+import arrowRight from '../assets/shared/desktop/icon-arrow-right.svg'
+
+import earphonesCategory from '../assets/shared/desktop/image-category-thumbnail-earphones.png'
+import headphonesCategory from '../assets/shared/desktop/image-category-thumbnail-headphones.png'
+import speakersCategory from '../assets/shared/desktop/image-category-thumbnail-speakers.png'
 
 export default {
   hamburgerMenu,
@@ -18,5 +23,9 @@ export default {
   cart,
   heroMobile,
   heroTablet,
-  hero
+  hero,
+  arrowRight,
+  earphonesCategory,
+  headphonesCategory,
+  speakersCategory
 }
