3be1802024fd8a82ff73d9cc254607082a4c4f17
Smith Ringtho <sringtho@gmail.com>
2023-06-13 13:59:12 +0000

    Added sections of the footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,57 @@
 import React from 'react'
 import './Footer.scss'
+import images from '../../constants/images'
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
   return (
-    <div>
-      Footer
+    <div className='footer__wrapper'>
+      <hr />
+      <div className='footer__nav'>
+        <div className='footer__image'>
+          <img src={images.logo} alt='logo' />
+        </div>
+        <div className='footer__ul'>
+          <ul className='footer__menu'>
+            <Link to='/'>
+              <li>Home</li>
+            </Link>
+            <Link to='/headphones'>
+              <li>Headphones</li>
+            </Link>
+            <Link to='/speakers'>
+              <li>Speakers</li>
+            </Link>
+            <Link to='/earphones'>
+              <li>Earphones</li>
+            </Link>
+          </ul>
+        </div>
+      </div>
+
+      <div className='footer__details'>
+        <p>
+        Audiophile is an all in one stop to fulfill your audio needs.
+        We&apos;re a small team of music lovers and sound specialists who
+        are devoted to helping you get the most out of personal audio.
+        Come and visit our demo facility - we’re open 7 days a week.
+        </p>
+        <div className='footer__copyright'>
+          <p>Copyright 2023. All Rights Reserved</p>
+
+          <div className='footer__buttons'>
+            <Link>
+              <img src={images.facebook} alt='facebook' />
+            </Link>
+            <Link>
+              <img src={images.twitter} alt='twitter' />
+            </Link>
+            <Link>
+              <img src={images.instagram} alt='instagram' />
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
